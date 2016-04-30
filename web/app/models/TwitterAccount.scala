4c71635bb9a6356b9aaddc95952d730c64eb0a7b
toshi <toshi@pixiv.com>
2016-04-30 11:49:13 +0900

    Use sql interpolation

--- web/app/models/TwitterAccount.scala
+++ web/app/models/TwitterAccount.scala
@@ -10,95 +10,39 @@ case class TwitterAccount(
   accessToken: String,
   accessTokenSecret: String)
 
-object TwitterAccount extends SQLSyntaxSupport[TwitterAccount] {
-
-  override val schemaName = Some("public")
-
-  override val tableName = "twitter_accounts"
-
-  override val columns = Seq("id", "screen_name", "access_token", "access_token_secret")
-
-  def apply(ta: SyntaxProvider[TwitterAccount])(rs: WrappedResultSet): TwitterAccount = apply(ta.resultName)(rs)
-  def apply(ta: ResultName[TwitterAccount])(rs: WrappedResultSet): TwitterAccount = new TwitterAccount(
-    id = rs.get(ta.id),
-    screenName = rs.get(ta.screenName),
-    accessToken = rs.get(ta.accessToken),
-    accessTokenSecret = rs.get(ta.accessTokenSecret)
-  )
-
-  val ta = TwitterAccount.syntax("ta")
-
-  def find(id: Long)(implicit session: DBSession): Option[TwitterAccount] = {
-    withSQL {
-      select.from(TwitterAccount as ta).where.eq(ta.id, id)
-    }.map(TwitterAccount(ta.resultName)).single.apply()
-  }
-
-  private def create(
-    id: Long,
-    screenName: String,
-    accessToken: String,
-    accessTokenSecret: String)(implicit session: DBSession): TwitterAccount = {
-    withSQL {
-      insert.into(TwitterAccount).columns(
-        column.id,
-        column.screenName,
-        column.accessToken,
-        column.accessTokenSecret
-      ).values(
-          id,
-          screenName,
-          accessToken,
-          accessTokenSecret
-        )
-    }.update.apply()
-
-    TwitterAccount(
-      id = id,
-      screenName = screenName,
-      accessToken = accessToken,
-      accessTokenSecret = accessTokenSecret)
-  }
-
-  private def save(entity: TwitterAccount)(implicit session: DBSession): TwitterAccount = {
-    withSQL {
-      update(TwitterAccount).set(
-        column.id -> entity.id,
-        column.screenName -> entity.screenName,
-        column.accessToken -> entity.accessToken,
-        column.accessTokenSecret -> entity.accessTokenSecret
-      ).where.eq(column.id, entity.id)
-    }.update.apply()
-    entity
-  }
-
-  def upsert(
-    id: Long,
-    screenName: String,
-    accessToken: String,
-    accessTokenSecret: String)(implicit session: DBSession): TwitterAccount = {
-    find(id) match {
-      case Some(_) =>
-        save(TwitterAccount(id, screenName, accessToken, accessTokenSecret))
-      case None =>
-        create(id, screenName, accessToken, accessTokenSecret)
-    }
-  }
-
-  def destroy(entity: TwitterAccount)(implicit session: DBSession): Unit = {
-    withSQL { delete.from(TwitterAccount).where.eq(column.id, entity.id) }.update.apply()
-  }
-
-}
-
 class TwitterAccountRepository @Inject() (connectionPool: ConnectionPool) {
 
-  implicit def session: DBSession = DBSession(conn = connectionPool.borrow())
+  private def db: DB = DB(connectionPool.borrow())
 
-  def find(id: Long): Option[TwitterAccount] = TwitterAccount.find(id)(session)
+  private def *(rs: WrappedResultSet): TwitterAccount = TwitterAccount(
+    id = rs.long("id"),
+    screenName = rs.string("screen_name"),
+    accessToken = rs.string("access_token"),
+    accessTokenSecret = rs.string("access_token_secret")
+  )
 
-  def upsert(id: Long, screenName: String, accessToken: String, accessTokenSecret: String): TwitterAccount = {
-    TwitterAccount.upsert(id, screenName, accessToken, accessTokenSecret)(session)
+  def find(id: Long): Option[TwitterAccount] = db.readOnly { implicit session =>
+    sql"select * from twitter_accounts where id = $id"
+      .map(*)
+      .single()
+      .apply()
+  }
+
+  def upsert(id: Long, screenName: String, accessToken: String, accessTokenSecret: String): TwitterAccount = db.localTx { implicit session =>
+    sql"""
+        INSERT INTO
+          twitter_accounts(id, screen_name, access_token, access_token_secret)
+        VALUES
+          ($id, $screenName, $accessToken, $accessTokenSecret)
+        ON CONFLICT (id) DO UPDATE
+        SET
+          screen_name = $screenName,
+          access_token = $accessToken,
+          access_token_secret = $accessTokenSecret
+      """
+      .update()
+      .apply()
+    TwitterAccount(id, screenName, accessToken, accessTokenSecret)
   }
 
 }
